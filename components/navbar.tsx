19498b4b6d072c6571fa8b47a2af18ccf81b5dfb
Vitagliano <gabriel.vitagliano@perfectflightapp.com>
2024-04-17 22:44:19 -0300

    add favicon, fix menu icon

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import {Button} from "@/components/ui/button"
 import {SheetTrigger, SheetContent, Sheet} from "@/components/ui/sheet"
 import Image from "next/image";
 import RetroButton from "@/components/retrobtn";
+import {Menu} from "lucide-react";
 
 export default function Navbar() {
     return (
@@ -16,7 +17,7 @@ export default function Navbar() {
                     <Sheet>
                         <SheetTrigger asChild>
                             <Button className="shrink-0 md:hidden" size="icon" variant="outline">
-                                <MenuIcon className="h-5 w-5"/>
+                                <Menu/>
                                 <span className="sr-only">Toggle navigation menu</span>
                             </Button>
                         </SheetTrigger>
@@ -103,23 +104,4 @@ export default function Navbar() {
     )
 }
 
-function MenuIcon(props) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <line x1="4" x2="20" y1="12" y2="12"/>
-            <line x1="4" x2="20" y1="6" y2="6"/>
-            <line x1="4" x2="20" y1="18" y2="18"/>
-        </svg>
-    )
-}
+
